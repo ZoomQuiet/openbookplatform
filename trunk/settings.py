4610ea37321012d5b6f47af9294ec32ffb7fc8a8
limodou <limodou@e9cb34ec-b428-0410-8744-11689a347de2>
2007-02-27 06:12:46 +0000

    update time-zone

--- trunk/settings.py
+++ trunk/settings.py
@@ -18,7 +18,7 @@ DATABASE_PORT = ''             # Set to empty string for default. Not used with 
 
 # Local time zone for this installation. All choices can be found here:
 # http://www.postgresql.org/docs/8.1/static/datetime-keywords.html#DATETIME-TIMEZONE-SET-TABLE
-TIME_ZONE = 'GMT+8'
+TIME_ZONE = 'GMT-8'
 
 # Language code for this installation. All choices can be found here:
 # http://www.w3.org/TR/REC-html40/struct/dirlang.html#langcodes
